d5946dcf9ba742f70f6bfab103dc8ce7a26384bf
karume-lab <danielkarume@outlook.com>
2026-09-24 21:15:43 +0300

    chore: move global.css to src to support absolute alias

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,12 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withUniwindConfig } = require("uniwind/metro");
 
 const config = getDefaultConfig(__dirname);
 
 module.exports = withUniwindConfig(config, {
   // relative path to your global.css file (from previous step)
-  cssEntryFile: './global.css',
+  cssEntryFile: "./src/global.css",
   // (optional) path where we gonna auto-generate typings
   // defaults to project's root
-  dtsFile: './uniwind-types.d.ts',
+  dtsFile: "./uniwind-types.d.ts",
 });
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import "../../global.css";
+import "@/global.css";
 
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
