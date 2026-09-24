0f4bd0624c124da78b15ba326038bf1ac3911f29
karume-lab <danielkarume@outlook.com>
2026-09-24 20:55:55 +0300

    chore: setup expo sqlite runtime migrations

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { ThemeProvider } from "expo-router/react-navigation";
 import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 import { useUniwind } from "uniwind";
 import { NAV_THEME } from "@/lib/theme";
 
@@ -12,8 +13,25 @@ export {
   ErrorBoundary,
 } from "expo-router";
 
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import { Text } from "@/components/ui/text";
+import { db } from "@/db/client";
+import migrations from "@/db/migrations/migrations";
+
 export default function RootLayout() {
   const { theme } = useUniwind();
+  const { success, error } = useMigrations(db, migrations);
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return null;
+  }
 
   return (
     <ThemeProvider value={NAV_THEME[theme ?? "light"]}>
